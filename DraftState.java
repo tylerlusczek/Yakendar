5d01b7f779b0e2d6f9bc812daaf3ff8a23c2a4af
zburke <zburke@vt.edu>
2025-12-04 11:36:35 -0500

    returns if publish or edit fails, added error message to publish

--- DraftState.java
+++ DraftState.java
@@ -14,6 +14,7 @@ public class DraftState implements PostState
         if(newContent == null)
         {
             System.out.println("Edit Failed: Not a valid edit");
+            return;
         }
         post.setContent(newContent);
         System.out.println("Post Successfully Edited");
@@ -22,6 +23,11 @@ public class DraftState implements PostState
     @Override
     public void publish(PostEntity post)
     {
+        if(post == null)
+        {
+            System.out.println("Publish Failed: Post doesn't exist");
+            return;
+        }
         PostState publish = new PublishedState();
         post.setState(publish);
         System.out.println("Post Successfully Published");
